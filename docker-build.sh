cf0778490053862f41c98d5be059930e6790c646
Ulf Lilleengen <lulf@redhat.com>
2017-05-29 12:59:22 +0200

    Push committed image

--- docker-build.sh
+++ docker-build.sh
@@ -17,9 +17,6 @@ pwd
 
 docker build --build-arg version=$VERSION -t $REPO:$COMMIT $DIR || exit 1
 
-if [ "$TRAVIS_BRANCH" == "master" ] || [ -n "$TRAVIS_TAG" ]
-then
-    docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
-    docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
-    docker push $REPO || exit 1
-fi
+docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
+docker push $REPO || exit 1
