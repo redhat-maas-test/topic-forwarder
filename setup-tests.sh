ea4985510e6c94a9dd69bfd4afa283582c0eeedd
Ulf Lilleengen <lulf@redhat.com>
2017-05-29 11:20:50 +0200

    Change install location of tests

--- setup-tests.sh
+++ setup-tests.sh
@@ -2,9 +2,9 @@
 git clone https://github.com/EnMasseProject/systemtests.git
 pushd systemtests
 echo "Current dir 1 $PWD"
-./scripts/install_openshift.sh openshift
+./scripts/install_openshift.sh /tmp/openshift
 echo "Current dir 2 $PWD"
-./scripts/start_openshift.sh openshift
+./scripts/start_openshift.sh /tmp/openshift
 echo "Current dir 3 $PWD"
 popd
 echo "Current dir 4 $PWD"
