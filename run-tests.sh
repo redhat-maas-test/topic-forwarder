f3f0010a97e822a8df252e910ad4d83ab9c42d15
Ulf Lilleengen <lulf@redhat.com>
2017-05-29 12:58:45 +0200

    Attempt at replacing with locally built image

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+COMPONENT=$1
+COMMIT=$2
+set -x
+
 pushd systemtests
 sleep 20
 export PATH=$PATH:/tmp/openshift
 cat /etc/hosts
-./scripts/run_test_travis.sh
+curl -L https://raw.githubusercontent.com/EnMasseProject/enmasse/master/install/openshift/enmasse.yaml -o enmasse.yaml
+sed -i -e "s/${COMPONENT}:latest/${COMPONENT}:${COMMIT}/g" enmasse.yaml
+
+./scripts/run_test_travis.sh enmasse.yaml
 ./scripts/print_logs.sh
 popd
